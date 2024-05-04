d7d02a32ab883ae8c8bc8c2340d90e30aa8dec61
Md-Kutub-Islam <mdkutubislam09@gamil.com>
2024-05-05 01:40:01 +0530

    Completed image processing, implemented getProduct function, and performed code upgrades

--- server/src/controllers/product.controller.js
+++ server/src/controllers/product.controller.js
@@ -2,30 +2,14 @@ import { asyncHandler } from '../utils/asyncHandler.js';
 import { ApiResponse } from '../utils/ApiResponse.js';
 import { ApiError } from '../utils/ApiError.js';
 import Product from '../models/product.model.js';
+import User from '../models/user.model.js';
+import { uploadOnCloudinary } from '../utils/cloudinary.js';
 
 export const createProduct = asyncHandler(async (req, res, next) => {
-  const {
-    name,
-    description,
-    size,
-    price,
-    category,
-    stock,
-    mainImage,
-    otherImage,
-    color,
-  } = req.body;
+  const { name, description, size, price, category, color } = req.body;
 
-  if (
-    !name ||
-    !description ||
-    !size ||
-    !price ||
-    !category ||
-    !mainImage ||
-    !color
-  ) {
-    return next(new ApiError('All field are required', 400));
+  if (!name || !description || !size || !price || !category || !color) {
+    return next(new ApiError('All fields are required', 400));
   }
 
   const product = await Product.create({
@@ -34,19 +18,45 @@ export const createProduct = asyncHandler(async (req, res, next) => {
     size,
     price,
     category,
-    stock,
-    mainImage,
-    otherImage,
+    stock: req.body.stock || 0, // Set stock to 0 if not provided
     color,
   });
 
   if (!product) {
     return next(
-      new ApiError('Product could not created, please try again', 500)
+      new ApiError('Failed to create product, please try again', 500)
     );
   }
 
-  res.status(200).json(new ApiResponse(200, 'ok', product));
+  return res
+    .status(201)
+    .json(new ApiResponse(201, 'Product created successfully', product));
+});
+
+export const getProduct = asyncHandler(async (req, res, next) => {
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  const products = await Product.find({}).skip(skip).limit(limit);
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, 'Products retrieved successfully', products));
+});
+
+export const getOneProduct = asyncHandler(async (req, res, next) => {
+  const { id } = req.params;
+
+  const product = await Product.findById(id);
+
+  if (!product) {
+    return next(new ApiError(`Product with id ${id} not found`, 404));
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, 'Product retrieved successfully', product));
 });
 
 export const updateProduct = asyncHandler(async (req, res, next) => {
@@ -66,9 +76,7 @@ export const updateProduct = asyncHandler(async (req, res, next) => {
   const product = await Product.findById(id);
 
   if (!product) {
-    return next(
-      new ApiError('Product could not created, please try again', 500)
-    );
+    return next(new ApiError('Product not found', 404));
   }
 
   if (name) {
@@ -101,7 +109,9 @@ export const updateProduct = asyncHandler(async (req, res, next) => {
 
   await product.save();
 
-  res.status(200).json(new ApiResponse(200, 'ok', product));
+  return res
+    .status(200)
+    .json(new ApiResponse(200, 'Product updated successfully', product));
 });
 
 export const removeProduct = asyncHandler(async (req, res, next) => {
@@ -109,12 +119,129 @@ export const removeProduct = asyncHandler(async (req, res, next) => {
   const product = await Product.findById(id);
 
   if (!product) {
-    return next(
-      new ApiError('Product could not created, please try again', 500)
-    );
+    return next(new ApiError('Product not found', 404));
   }
 
   await Product.findByIdAndDelete(id);
 
-  res.status(200).json(new ApiResponse(200, 'Product deleted successfully'));
+  return res
+    .status(200)
+    .json(new ApiResponse(200, 'Product deleted successfully'));
+});
+
+export const uploadMainImage = asyncHandler(async (req, res) => {
+  const uploadedFile = req.file;
+  const { productId } = req.params;
+  const { id } = req.params;
+
+  const proDuct = await Product.findById(productId);
+
+  const user = await User.findById(id);
+
+  if (!user || user.role !== availableUserRoles.ADMIN) {
+    throw new ApiError(403, 'Only admin users can upload product images');
+  }
+
+  if (!uploadedFile) {
+    throw new ApiError(400, 'No file uploaded');
+  }
+
+  const uploadOptions = {
+    folder: 'mainImage',
+    width: 800,
+    height: 600,
+    crop: 'fit',
+  };
+
+  const img = await uploadOnCloudinary(uploadedFile.path, uploadOptions);
+
+  if (!img) {
+    throw new ApiError(500, 'Something went wrong while uploading the image');
+  }
+
+  if (img.http_code === 400) {
+    throw new ApiError(500, `Error uploading image: ${img.message}`);
+  }
+
+  const imageData = {
+    url: img.url,
+    public_id: img.public_id,
+    secure_url: img.secure_url,
+    width: img.width,
+    height: img.height,
+    format: img.format,
+  };
+
+  if (imageData) {
+    proDuct.mainImage = imageData;
+  }
+
+  await proDuct.save();
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, 'Image uploaded successfully', { image: imageData })
+    );
+});
+
+export const uploadOtherImages = asyncHandler(async (req, res) => {
+  const uploadedFiles = req.files;
+  const { productId } = req.params;
+  const { id } = req.params;
+  const imageDataArray = [];
+
+  const product = await Product.findById(productId);
+  const user = await User.findById(id);
+
+  if (!user || user.role !== availableUserRoles.ADMIN) {
+    throw new ApiError(403, 'Only admin users can upload product images');
+  }
+
+  if (!product) {
+    throw new ApiError(404, 'Product not found');
+  }
+
+  if (!uploadedFiles || uploadedFiles.length === 0) {
+    throw new ApiError(400, 'No files uploaded');
+  }
+
+  const uploadOptions = {
+    folder: 'otherImages',
+    width: 800,
+    height: 600,
+    crop: 'fit',
+  };
+
+  for (const uploadedFile of uploadedFiles) {
+    const img = await uploadOnCloudinary(uploadedFile.path, uploadOptions);
+
+    if (!img) {
+      throw new ApiError(500, 'Something went wrong while uploading the image');
+    }
+
+    if (img.http_code === 400) {
+      throw new ApiError(500, `Error uploading image: ${img.message}`);
+    }
+
+    const imageData = {
+      url: img.url,
+      public_id: img.public_id,
+      secure_url: img.secure_url,
+      width: img.width,
+      height: img.height,
+      format: img.format,
+    };
+
+    imageDataArray.push(imageData);
+  }
+
+  product.otherImages.push(...imageDataArray);
+  await product.save();
+
+  return res.status(200).json(
+    new ApiResponse(200, 'Images uploaded successfully', {
+      images: imageDataArray,
+    })
+  );
 });
--- server/src/routes/product.routes.js
+++ server/src/routes/product.routes.js
@@ -3,12 +3,29 @@ import {
   createProduct,
   updateProduct,
   removeProduct,
+  uploadMainImage,
+  uploadOtherImages,
+  getProduct,
+  getOneProduct,
 } from '../controllers/product.controller.js';
+import { upload } from '../middlewares/multer.middleware.js';
 
 const router = Router();
 
-router.route('/').post(createProduct);
+router.route('/').post(createProduct).get(getProduct);
 
-router.route('/:id').put(updateProduct).delete(removeProduct);
+router
+  .route('/:id')
+  .get(getOneProduct)
+  .put(updateProduct)
+  .delete(removeProduct);
+
+router
+  .route('/mainImage/:productId')
+  .post(upload.single('mainImage'), uploadMainImage);
+
+router
+  .route('/otherImages/:productId')
+  .post(upload.array('otherImages', 4), uploadOtherImages);
 
 export default router;
