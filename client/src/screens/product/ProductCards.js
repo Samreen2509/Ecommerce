746072ddb8382d85b861bf85ba02f63fc81c2e54
Aditya-Deshmukh9 <adityadesh937@gmail.com>
2024-05-14 21:27:52 +0530

    add changes

--- client/src/screens/product/ProductCards.js
+++ client/src/screens/product/ProductCards.js
@@ -5,16 +5,16 @@ const ProductCard = (props) => {
     <div className="m-10 flex h-80 w-64 flex-wrap place-content-center justify-center rounded-md ">
       <div>
         <img
-          className="h-64 w-64 border-2 border-solid hover:border-gray-600 object-contain"
+          className="h-64 w-64 border-2 border-solid object-contain hover:border-gray-600"
           alt="cardImg"
           src={image}
         />
       </div>
       <div>
-        <h3 className="w-64 my-4 ">
-        {title} - {price}
-      </h3>
-    </div>
+        <h3 className="my-4 w-64 ">
+          {title} - {price}
+        </h3>
+      </div>
     </div>
   );
 };
